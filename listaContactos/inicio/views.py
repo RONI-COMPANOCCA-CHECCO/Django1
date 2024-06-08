38be3e1f458ad7558a7b2c79b4e2595fb18df8da
rcompanocca <ronicompanocca@gmail.com>
2024-06-08 18:59:13 -0500

    agregue una nueva vista para la URL another

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
     return HttpResponse("<h1>HOLA MUNDO DESDE DJANGO</h1>")
+
+def anotherView(request):
+    return HttpResponse("<h1>Solo otra pagina</h1>")
