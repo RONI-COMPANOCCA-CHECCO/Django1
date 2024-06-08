5c1a2d798f2c55966343a8f7034bc112b141068c
rcompanocca <ronicompanocca@gmail.com>
2024-06-08 16:53:21 -0500

    definimos la longitud maxima de un campo de tipo textfield

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
+    nombres = models.TextField(max_length=100)
+    apellidos = models.TextField(max_length=100)
     edad = models.TextField()
