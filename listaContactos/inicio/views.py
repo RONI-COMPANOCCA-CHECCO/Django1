bb7ad6075b4da24bc47397243918c19edbc4a1c4
rcompanocca <ronicompanocca@gmail.com>
2024-06-08 17:49:52 -0500

    agregue en views.py en el app inicio una linea de html

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def myHomeView(*args, **kwargs):
+    return HttpResponse("<h1>HOLA MUNDO DESDE DJANGO</h1>")
