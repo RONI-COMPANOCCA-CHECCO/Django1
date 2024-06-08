cd70e89fe2e127c16a4ebb18baef2b88e4e0f288
rcompanocca <ronicompanocca@gmail.com>
2024-06-08 17:07:18 -0500

    añadimos un nuevo campo, en el registro

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Persona(models.Model):
     nombres = models.TextField(max_length=100)
     apellidos = models.TextField(max_length=100)
-    edad = models.TextField()
+    edad = models.IntegerField()#(max_digits=3)
