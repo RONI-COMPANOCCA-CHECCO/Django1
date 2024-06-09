ed0f61b1cee85e413acfe1f51db8d281573a74a6
rcompanocca <ronicompanocca@gmail.com>
2024-06-08 19:03:27 -0500

    las funciones de las vistas reciben un argumento

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
     return HttpResponse("<h1>HOLA MUNDO DESDE DJANGO</h1>")
 
 def anotherView(request):
