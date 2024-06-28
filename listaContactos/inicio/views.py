46f7e07e0b99ca899207e54d1a51c6596aa2f90e
rcompanocca <ronicompanocca@gmail.com>
2024-06-28 12:48:52 -0500

    Empezamos con django 3

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -6,14 +6,5 @@ def myHomeView(request, *args, **kwargs):
     print(request.user)
     return render(request, "home.html")
 
-def anotherView(request):
-    return HttpResponse("<h1>Solo otra pagina</h1>")
-
-def informacionView(request):
-    return HttpResponse("<h2>Acerca de Nosotros</h2><p>Esta es una página de información sobre nosotros.</p>")
-
-def contactoView(request):
-    return HttpResponse("<h2>Contacto</h2><p>Encuéntranos en Av. Venezuela N 725.</p>")
-
-def productosView(request):
-    return HttpResponse("<h2>Nuestros Productos</h2><p>Echa un vistazo a nuestra increíble selección de productos.</p>")
+def about_view(request):
+    return render(request, 'about.html')
--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
-from inicio.views import anotherView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
