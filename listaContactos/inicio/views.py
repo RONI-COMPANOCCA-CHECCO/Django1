2fa16747ebb924eae0a288b7adaefcea2f5a1c40
rcompanocca <ronicompanocca@gmail.com>
2024-06-28 15:01:54 -0500

    los datos enviados en el contexto, para roducir resultado vissible

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def myHomeView(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return render(request, "home.html")
+    return render(request, "home.html", {})
 
 def about_view(request):
     return render(request, 'about.html')
