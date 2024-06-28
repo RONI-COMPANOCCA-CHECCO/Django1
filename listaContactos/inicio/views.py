8d1d3ddbeb76dce0764d5ebe4bc991924d407f8a
rcompanocca <ronicompanocca@gmail.com>
2024-06-28 15:28:03 -0500

    Hago las variables de contexto, en un template, y envio variables desde la vista, y en mi plantilla esos elementos son tratados como variables

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def myHomeView(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    return render(request, "home.html", {})
+    myContext ={
+        "myText":"Esto es sobre nosotros.",
+        "myNumber": 123,
+    }
+    return render(request, "home.html", myContext)
 
-def about_view(request):
-    return render(request, 'about.html')
+#def about_view(request):
+    #return render(request, 'about.html')
