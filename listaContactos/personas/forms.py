ce4c8538aeb9f769fcd6639c7aff5954a622a6b9
rcompanocca <ronicompanocca@gmail.com>
2024-07-10 17:09:51 -0500

    Editamos forms.py y agregamos un nuevo metodo para hacerlo invalido

--- listaContactos/personas/forms.py
+++ listaContactos/personas/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from .models import Persona
 
-class Personaform(forms.ModelForm):
+class PersonaForm(forms.ModelForm):
     class Meta:
         model = Persona
-        fields = [
-            "nombres",
-            "apellidos",
-            "edad",
-            "donador",
-        ]
+        fields = ['nombres', 'apellidos', 'edad', 'donador']
+
+    def clean_edad(self):
+        edad = self.cleaned_data.get('edad')
+        if edad <= 0:
+            raise forms.ValidationError("La edad debe ser un número positivo.")
+        return edad
