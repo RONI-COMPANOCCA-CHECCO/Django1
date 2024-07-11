3e13ac552dbb52cab2969d3a711ebfb36742f1f0
rcompanocca <ronicompanocca@gmail.com>
2024-07-10 22:27:23 -0500

    editando views.py

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Persona
-from .forms import PersonaForm
+from .forms import PersonaForm, RawPersonaForm
 
 def personaTestView(request):
     obj = Persona.objects.get(id=1)
@@ -10,17 +10,10 @@ def personaTestView(request):
     return render(request, "personas/test.html", context)
 
 def personaCreateView(request):
-    form = PersonaForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = PersonaForm()  
-    else:
-        print(form.errors)  
-
-    context = {
-        "form": form
+    form = RawPersonaForm()
+    context ={
+        "form": form,
     }
     return render(request, "personas/personasCreate.html", context)
-
 def searchForHelp(request):
     return render(request, "personas/search.html", {})
